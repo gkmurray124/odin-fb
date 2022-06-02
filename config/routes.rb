bd71633adb693504d1e16e7f953078d80117b90d
Grace Murray <grace.murray@dispatchit.com>
2022-06-02 10:23:45 -0700

    User model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'posts#index'
+
+  resources :users, only: :show do 
+    get 'posts'
+  end 
+
   resources :posts
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_22_211110) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_172200) do
   create_table "posts", force: :cascade do |t|
     t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.date "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
